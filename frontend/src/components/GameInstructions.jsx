1b69fbfcc6b8d63f17ff92eea9efb9df2c9b5087
vishal5208 <vishald5208@gmail.com>
2024-01-14 13:08:36 +0530

    updated game

--- frontend/src/components/GameInstructions.jsx
+++ frontend/src/components/GameInstructions.jsx
@@ -1,6 +1,5 @@
-// GameInstructions.js
 import React from 'react';
-import { Card, CardContent, Typography, Button } from '@mui/material';
+import { Card, CardContent, Typography } from '@mui/material';
 
 const GameInstructions = () => {
     return (
@@ -17,19 +16,19 @@ const GameInstructions = () => {
                         <span style={{ color: 'red' }}>Vishal</span> creates a game and invites <span style={{ color: 'green' }}>Aditi</span> to play.
                     </li>
                     <li style={{ fontSize: '1.3rem' }}>
-                        <span style={{ color: 'green' }}>Aditi</span> joins the game by pressing the "Join An Existing Game" button, staking the same amount as <span style={{ color: 'red' }}>Vishal</span>.
+                        <span style={{ color: 'green' }}>Aditi</span> joins the game by staking the same amount as <span style={{ color: 'red' }}>Vishal</span>.
                     </li>
                     <li style={{ fontSize: '1.3rem' }}>
-                        If <span style={{ color: 'green' }}>Aditi</span> misses the move deadline, <span style={{ color: 'red' }}>Vishal</span> can click the timeout button to reclaim his staked ETH.
+                        If <span style={{ color: 'green' }}>Aditi</span> misses the move deadline, <span style={{ color: 'red' }}>Vishal</span> can reclaim his staked ETH.
                     </li>
                     <li style={{ fontSize: '1.3rem' }}>
-                        If <span style={{ color: 'green' }}>Aditi</span> plays but <span style={{ color: 'red' }}>Vishal</span> doesn't reveal his move, <span style={{ color: 'green' }}>Aditi</span> can press the timeout button to claim both stakes.
+                        If <span style={{ color: 'green' }}>Aditi</span> plays but <span style={{ color: 'red' }}>Vishal</span> doesn't reveal his move, <span style={{ color: 'green' }}>Aditi</span> can claim both stakes.
                     </li>
                     <li style={{ fontSize: '1.3rem' }}>
                         After revealing his move, the winner will receive the total staked ETH (twice the staked amount). In case of a draw, where no one wins, both players will get their staked ETH back.
                     </li>
                     <li style={{ fontSize: '1.3rem' }}>
-                        At the end of the game, if both players have made their moves, <span style={{ color: 'red' }}>Vishal</span> can reveal his move by clicking the 'Resolve Game' button.
+                        At the end of the game, if both players have made their moves, <span style={{ color: 'red' }}>Vishal</span> can reveal his move.
                     </li>
                 </ol>
             </CardContent>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,48 +1,24 @@
 import React, { useState } from 'react';
 import { Typography, Button, Grid, Box } from '@mui/material';
 import CreateGame from './CreateGame';
-import JoinGame from './JoinGame';
-import ResolveGame from './ResolveGame';
-import TimeoutModal from './TimeoutModal';
-import GameHistory from './GameHistory';
-import { green, blue, orange, red } from '@mui/material/colors';
 import GameInstructions from './GameInstructions';
+import { green } from '@mui/material/colors';
 
+import GameHistory from './GameHistory';
 
 const Home = () => {
     const [isCreateGameModalOpen, setCreateGameModalOpen] = useState(false);
-    const [isJoinGameModalOpen, setJoinGameModalOpen] = useState(false);
-    const [isResolveGameModalOpen, setResolveGameModalOpen] = useState(false);
-    const [j1TimeoutOpen, setJ1TimeoutOpen] = useState(false);
 
     const handleOpenCreateGameModal = () => {
         setCreateGameModalOpen(true);
     };
+
     const handleCloseCreateGameModal = () => {
         setCreateGameModalOpen(false);
-    };
-    const handleOpenJoinGameModal = () => {
-        setJoinGameModalOpen(true);
-    };
-    const handleCloseJoinGameModal = () => {
-        setJoinGameModalOpen(false);
-    };
-    const handleOpenResolveGameModal = () => {
-        setResolveGameModalOpen(true);
-    };
-    const handleCloseResolveGameModal = () => {
-        setResolveGameModalOpen(false);
-    };
-    const handleTimeoutOpen = () => {
-        setJ1TimeoutOpen(true);
-    };
-    const handleTimeoutClose = () => {
-        setJ1TimeoutOpen(false);
     };
 
     return (
         <div>
-
             <Typography variant="h3" style={{ marginBottom: '20px', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 Welcome to Rock, Paper, Scissors, Spock, Lizard Game Center
             </Typography>
@@ -51,73 +27,41 @@ const Home = () => {
             <div style={{ margin: '20px' }}></div>
 
             <Grid container spacing={2} alignItems="center" justifyContent="center">
-                {[1, 2, 3, 4].map((index) => (
-                    <Grid key={index} item xs={12} md={6}>
-                        <Box
-                            border={1}
-                            p={2}
-                            borderRadius={5}
-                            height="150px"
-                            display="flex"
-                            flexDirection="column"
-                            justifyContent="space-between"
-                            marginBottom={2}
+                <Grid item xs={12} md={6}>
+                    <Box
+                        border={1}
+                        p={2}
+                        borderRadius={5}
+                        height="150px"
+                        display="flex"
+                        flexDirection="column"
+                        justifyContent="space-between"
+                        marginBottom={2}
+                    >
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            fullWidth
+                            onClick={handleOpenCreateGameModal}
+                            style={{
+                                fontSize: '1.2rem',
+                                background: green[500],
+                                color: 'white', // Text color is set to white
+                            }}
                         >
-                            <Button
-                                variant="contained"
-                                color={index === 1 ? 'primary' : index === 2 ? 'secondary' : index === 3 ? 'info' : 'warning'}
-                                size="large"
-                                fullWidth
-                                onClick={
-                                    index === 1
-                                        ? handleOpenCreateGameModal
-                                        : index === 2
-                                            ? handleOpenJoinGameModal
-                                            : index === 3
-                                                ? handleOpenResolveGameModal
-                                                : handleTimeoutOpen
-                                }
-                                style={{
-                                    fontSize: '1.2rem',
-                                    background:
-                                        index === 1 ? green[500] :
-                                            index === 2 ? blue[500] :
-                                                index === 3 ? orange[500] :
-                                                    red[500],
-                                    color: 'white', // Text color is set to white
-                                }}
-                            >
-                                {index === 1
-                                    ? 'Start a New Game'
-                                    : index === 2
-                                        ? 'Join an Existing Game'
-                                        : index === 3
-                                            ? 'Resolve an Ongoing Game'
-                                            : 'Timeout Resolution'}
-                            </Button>
-                            <Typography variant="body1" style={{ marginLeft: '10px', fontSize: '1.2rem', lineHeight: '1.2', textAlign: 'center' }}>
-                                {index === 1
-                                    ? 'Challenge someone by creating a new game. Select your move and wait for an opponent to join.'
-                                    : index === 2
-                                        ? 'Enter a game ID to join an ongoing challenge. Make your move and see who wins!'
-                                        : index === 3
-                                            ? 'Provide details to resolve an ongoing challenge. Ensure fair play and determine the winner.'
-                                            : 'Resolve a game due to timeout by entering the game ID. Address situations where players couldn\'t make a move in time.'}
-                            </Typography>
-                        </Box>
-
-                    </Grid>
-                ))}
+                            Start a New Game
+                        </Button>
+                        <Typography variant="body1" style={{ marginLeft: '10px', fontSize: '1.2rem', lineHeight: '1.2', textAlign: 'center' }}>
+                            Challenge someone by creating a new game. Select your move and wait for an opponent to join.
+                        </Typography>
+                    </Box>
+                </Grid>
             </Grid>
 
+            <CreateGame open={isCreateGameModalOpen} handleClose={handleCloseCreateGameModal} />
             <GameHistory />
 
-            <CreateGame open={isCreateGameModalOpen} handleClose={handleCloseCreateGameModal} />
-
-            <JoinGame open={isJoinGameModalOpen} handleClose={handleCloseJoinGameModal} />
-
-            <ResolveGame open={isResolveGameModalOpen} handleClose={handleCloseResolveGameModal} />
-            <TimeoutModal open={j1TimeoutOpen} handleClose={handleTimeoutClose} />
         </div>
     );
 };
